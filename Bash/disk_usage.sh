e571f86694c7ef8c5e4f9859852a0ee56a203d26
JWT890 <jwtaylor659@gmail.com>
2025-07-28 19:17:45 -0500

    commenting code

--- Bash/disk_usage.sh
+++ Bash/disk_usage.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
+# checks to see if the script is being run as root
 if [[ $EUID -ne 0 ]]; then
+    # if not root, prints it must be run as root
     echo "This script must be run as root" >&2
     exit 1
 fi
 
+# creates the log file
 LOG_FILE= "storage_report.txt"
 
+# prints to the report file
 echo "-------------------------" >> $LOG_FILE
+# prints to the report file
 echo "Disk Usage Report" >> $LOG_FILE
+# prints to the report file
 echo "-------------------------" >> $LOG_FILE
 
+# checks disk usage for a specific partition
+# you can change the partition as needed
 PARTITION="/dev/sda1"
 
-DISK_USAGE = $(df -h | grep "$PARTITION" | awk '{print $5}' | tr -d '%')
+# gets the disk usage from the paritition and the percentage used
+disk_usage=$(df -h | grep "$PARTITION" | awk '{print $5}' | tr -d '%')
 
+# set to 90 for critical level
 CRITICAL_THRESHOLD=90
+# set to 75 for warning level
 WARNING_THRESHOLD=75
 
+# checks for if the critical threshold has been met or exceeded
 if [[ $DISK_USAGE -ge $CRITICAL_THRESHOLD ]]; then
-    echo "CRITICAL: Disk usage on $DISK_USAGE%: Immediate action required!" >> $LOG_FILE
+    # prints it has and must be addressed and prints to the report file
+    echo "CRITICAL: Disk usage on $disk_usage%: Immediate action required!" >> $LOG_FILE
+# checks for if the warning threshold has been met or reached
 elif [[ $DISK_USAGE -ge $WARNING_THRESHOLD ]]; then
-    echo "WARNING: Disk usage on $DISK_USAGE%: Consider cleaning up." >> $LOG_FILE
+    # prints it has and must consider cleaning up resources and prints to the report file
+    echo "WARNING: Disk usage on $disk_usage%: Consider cleaning up." >> $LOG_FILE
 else
-    echo "OK: Disk usage on $DISK_USAGE%." >> $LOG_FILE
+    # prints that nothing of note needs to be done and prints to the report file
+    echo "OK: Disk usage on $disk_usage%." >> $LOG_FILE
 fi
