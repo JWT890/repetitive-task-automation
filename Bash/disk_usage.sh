fe05b8fda69d5666a88a36a4b81872bde36ede26
JWT890 <jwtaylor659@gmail.com>
2025-07-28 19:05:12 -0500

    update

--- Bash/disk_usage.sh
+++ Bash/disk_usage.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-LOG_FILE= storage_report.txt
+LOG_FILE= "storage_report.txt"
 
 echo "-------------------------" >> $LOG_FILE
 echo "Disk Usage Report" >> $LOG_FILE
@@ -13,15 +13,15 @@ echo "-------------------------" >> $LOG_FILE
 
 PARTITION="/dev/sda1"
 
-disk_usage = $(df -h | grep "$PARTITION" | awk '{print $5}' | tr -d '%')
+DISK_USAGE = $(df -h | grep "$PARTITION" | awk '{print $5}' | tr -d '%')
 
 CRITICAL_THRESHOLD=90
 WARNING_THRESHOLD=75
 
 if [[ $DISK_USAGE -ge $CRITICAL_THRESHOLD ]]; then
-    echo "CRITICAL: Disk usage on $disk_usage%! Immediate action required!" >> $LOG_FILE
+    echo "CRITICAL: Disk usage on $DISK_USAGE%: Immediate action required!" >> $LOG_FILE
 elif [[ $DISK_USAGE -ge $WARNING_THRESHOLD ]]; then
-    echo "WARNING: Disk usage on $disk_usage%! Consider cleaning up." >> $LOG_FILE
+    echo "WARNING: Disk usage on $DISK_USAGE%: Consider cleaning up." >> $LOG_FILE
 else
-    echo "OK: Disk usage on $disk_usage%." >> $LOG_FILE
+    echo "OK: Disk usage on $DISK_USAGE%." >> $LOG_FILE
 fi
