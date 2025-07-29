87ce541dc083905be475715862b59b641406d454
JWT890 <jwtaylor659@gmail.com>
2025-07-28 21:39:42 -0500

    update

--- Bash/disk_usage.sh
+++ Bash/disk_usage.sh
@@ -10,7 +10,7 @@ check_disk_usage() {
     fi
 
     # creates the log file
-    LOG_FILE= "storage_report.txt"
+    LOG_FILE="storage_report.txt"
 
     # prints to the report file
     echo "-------------------------" >> "${LOG_FILE}"
@@ -32,11 +32,11 @@ check_disk_usage() {
     WARNING_THRESHOLD=75
 
     # checks for if the critical threshold has been met or exceeded
-    if [[ $DISK_USAGE -ge $CRITICAL_THRESHOLD ]]; then
+    if [[ $disk_usage -ge $CRITICAL_THRESHOLD ]]; then
         # prints it has and must be addressed and prints to the report file
         echo "CRITICAL: Disk usage on $disk_usage%: Immediate action required!" >> "${LOG_FILE}"
     # checks for if the warning threshold has been met or reached
-    elif [[ $DISK_USAGE -ge $WARNING_THRESHOLD ]]; then
+    elif [[ $disk_usage -ge $WARNING_THRESHOLD ]]; then
         # prints it has and must consider cleaning up resources and prints to the report file
         echo "WARNING: Disk usage on $disk_usage%: Consider cleaning up." >> "${LOG_FILE}"
     else
