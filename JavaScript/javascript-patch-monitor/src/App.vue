264f76c5e19cedb928d1d0df0453f9c572bf5ea8
JWT890 <jwtaylor659@gmail.com>
2025-12-13 13:24:00 -0600

    updating to add javascript and c++

---
+++ JavaScript/javascript-patch-monitor/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <AutomatedPatchMonitor />
+</template>
+
+<script>
+  import AutomatedPatchMonitor from './components/AutomatedPatchMonitor.vue';
+  export default {
+    name: 'App',
+    components: {
+      AutomatedPatchMonitor
+    }
+  }
+</script>
+
