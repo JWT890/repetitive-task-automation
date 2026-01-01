324eb7a4213f669ff564870135f75845b5da433c
JWT890 <jwtaylor659@gmail.com>
2026-01-01 16:08:20 -0600

    testing in linux. will add to other repo

--- Bash/linux_server_patch_automation.sh
+++ Bash/linux_server_patch_automation.sh
@@ -54,5 +54,51 @@ install_packages() {
     echo "Installation completed: $(date)" >> $LOG_FILE
 }
 remove_packages() {
-    
+    $LOG_FILE='/var/log/system_updates.log'
+    echo "Checking for packages to remove..."
+    case "$(lsb_release is | tr -s ' ' | cut -d ' ' -f2)" in
+        Debian*)
+            sudo apt autoremove -y
+            ;;
+        Ubuntu*)
+            sudo apt autoremove -y
+            ;;
+        RHEL*)
+            sudo yum autoremove -y
+            ;;
+        CentOS*)
+            sudo apt autoremove -y
+            ;;
+        *)
+            echo "Unsupported. Skipping"
+    esac
+    echo "Unused packages removed successfully"
+    echo "Completed: $(date)" >> $LOG_FILE
 }
+
+echo "Starting patch automation script..."
+OPTIONS=("Check Linux System" "Install Packages" "Remove Packages" "Exit")
+select choice in "$(OPTIONS[@])"
+do
+    case $choice in
+        "Check Linux System")
+            check_linux_system
+            break
+            ;;
+        "Install Packages")
+            install_packages
+            break
+            ;;
+        "Remove Packages")
+            remove_packages
+            break
+            ;;
+        "Exit")
+            echo "Exiting the script"
+            exit 0
+            ;;
+        *)
+            echo "Invalid option, please try again."
+            ;;
+    esac
+done
